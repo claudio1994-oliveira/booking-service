93491426f3869b1702a1b83767482b362db74772
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-05-02 21:59:40 -0300

    creating schedule an exclusions

---
+++ app/Models/Schedule.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Schedule extends Model
+{
+    use HasFactory;
+
+    public function casts()
+    {
+        return [
+            'starts_at' => 'date',
+            'ends_at' => 'date',
+
+        ];
+    }
+}
