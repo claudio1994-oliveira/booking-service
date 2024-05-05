f1a67b0c700a546812688e7793f5b4e998b1300b
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-05-05 16:28:30 -0300

    customize the calendar of checkout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>{{ $title ?? config('app.name') }}</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
 </head>
 
 <body>
@@ -15,6 +15,7 @@
     </div>
 
 </body>
-@vite('resources/css/app.js')
+
+@livewireScriptConfig
 
 </html>
